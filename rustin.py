7154eb3a25d21ff8c32e5840025ba4da11dde7fb
Rustin Shamloo <rustinshamloo@Rustins-MacBook-Pro.local>
2020-12-20 18:00:22 -0800

    updated

--- rustin.py
+++ rustin.py
@@ -1,4 +1,8 @@
-import csv
+
+# edit in here, run in terminal, save/push to git on terminal:
+#1. git add .
+#2. git commit -m "message"
+#3. git push
 
 # create function that returns player characteristics (height,weight,etc.) with playerid as input
 # the data is in players.csv
@@ -6,9 +10,34 @@ import csv
 # might have to convert from str to flt, etc.
 
 
+import csv
+# This function returns the characteristics of a player given the nfl id.
+def stats(nflId):
+    with open('../nfl-big-data-bowl-2021/players.csv') as f:
+        mycsv = csv.reader(f)
+        for row in mycsv:
+            playerid = row[0]
+            if(current_nflId == nflId):
+                player = row
+        return [str(player[6]), float(player[1]), float(player[2]), str(player[3]), str(player[4]),
+                str(player[5])]
+
+
+
 #next, create function that returns their playerid's, similar to all game ids function
 
-# edit in here, run in terminal, save/push to git on terminal:
-#1. git add .
-#2. git commit -m "message"
-#3. git push
+def all_player_ids():
+    player_ids = []
+    with open('../nfl-big-data-bowl-2021/players.csv') as f:
+        mycsv = csv.reader(f)
+        for row in mycsv:
+            if(row[0] not in player_ids):
+                player_ids.append(row[0])
+    return player_ids
+
+print(all_player_ids())
+
+
+
+
+
